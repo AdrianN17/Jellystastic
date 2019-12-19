f5e5c1d28a18c9d615a807df740fb963ef75e27f
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-12-19 09:58:33 -0500

    0.0.3
    Ediciones minimas

--- gamestates/game_conf.lua
+++ gamestates/game_conf.lua
@@ -13,6 +13,7 @@ function game_conf:init(nombreMapa)
   self.cam = gamera.new(0,0,self.map.width*self.map.tilewidth, self.map.height*self.map.tileheight)
   self.cam:setWindow(0,0,x,y)
   
+  self:map_read();
 end
 
 function game_conf:update_conf(dt)
@@ -21,6 +22,49 @@ end
 
 function game_conf:draw_conf()
   self.map:draw()
+end
+
+
+function game_conf:map_read()
+
+  for _, layer in ipairs(self.map.layers) do
+    if layer.type=="tilelayer" then
+      --self:get_tile(layer)
+    elseif layer.type=="objectgroup" then
+      self:get_objects(layer)
+    end
+  end
+  
+  --self.map:removeLayer("Borrador")
+  
+end
+
+function game_conf:get_objects(objectlayer)
+  
+    for _, obj in pairs(objectlayer.objects) do
+      if obj.name then
+        
+        local data_pos = nil
+        
+        if obj.shape =="point" then
+          data_pos = {obj.x,obj.y}
+        elseif obj.shape == "polygon" then
+          local polygon = {}
+
+          for _,data in ipairs(obj.polygon) do
+            table.insert(polygon,data.x)
+            table.insert(polygon,data.y)
+          end
+          
+          data_pos = polygon
+        end
+        
+        if data_pos ~= nil then
+        
+        end
+        
+      end
+    end
 end
 
 
