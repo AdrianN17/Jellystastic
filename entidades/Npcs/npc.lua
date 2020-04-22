b0c3328fb354202a8b05ca9979ad20ec4ae99d6e
Adrian <36133718+AdrianN17@users.noreply.github.com>
2020-04-22 17:15:24 -0500

    Update npc.lua

--- entidades/Npcs/npc.lua
+++ entidades/Npcs/npc.lua
@@ -1,6 +1,6 @@
 local Timer = require "libs.chrono.Timer"
 local remove = require "entidades.remove"
-local visible = require "entidades.visible" 
+local visible = require "entidades.visible"
 
 local npc = Class{
   __includes = {remove,visible}
@@ -10,48 +10,48 @@ function npc:init(entidad,body,shape,fixture,ox,oy,radio,shapeTableClear,propert
   self.body = body
   self.shape = shape
   self.fixture = fixture
-  
+
   self.shapeTableClear = shapeTableClear
   self.properties = properties
-  
+
   self.entidad = entidad
-  
+
   self.radio = radio
-  
+
   self.grupo = properties.grupo
   self.hp = properties.hp
   self.maxHp = self.hp
-  
+
   self.spritesheet = Index_img[properties.img]
-  
+
   self.img = self.spritesheet.img
   self.quad = self.spritesheet.quad
   self.dimension = self.spritesheet.viewport
-  
+
   self.iterador = 1
   self.iteradorEstado = 1
-  
+
   entidad:add(properties.tabla,self)
-  
+
   self.timer = Timer()
-  
+
   remove.init(self,entidad,properties.tabla)
-  
+
   self.width,self.height = width,height
-  
+
   self.ox,self.oy = ox,oy
-  
+
   self.Es_salvable = properties.Es_salvable
-  
+
   self.vec4Shader = {properties.h or 0,properties.s or 0,0,0}
-  
+
   self.shaderPlayer = love.graphics.newShader(Shader_index.shader_player)
   self.shaderPlayer:send("color_player",self.vec4Shader)
-  
+
   self.acciones = {invulnerable =false}
-  
+
   self.cooldownIterador = true
-  
+
   visible.init(self)
 end
 
@@ -59,7 +59,7 @@ function npc:draw()
   local dimension = self.dimension[1][1]
 
   local wi,hi = self.width/dimension.w,self.height/dimension.h 
-  
+
   love.graphics.setShader(self.shaderPlayer)
     love.graphics.draw(self.img,self.quad[self.iteradorEstado][self.iterador],self.ox,self.oy,self.radio,wi,hi,dimension.w/2,dimension.h/2)
   love.graphics.setShader()
@@ -68,18 +68,18 @@ end
 function npc:update(dt)
   self.ox,self.oy = self.body:getX(),self.body:getY()
   self.radio = self.body:getAngle()
-  
+
   self.timer:update(dt)
-  
+
   self:checkVida()
-  
+
 end
 
 function npc:cambiarEstado(tipo)
   local hp = self.maxHp*0.5
 
   if self.hp<hp and self.cooldownIterador then
-    
+
     if tipo == "semizombie" then
       self.iteradorEstado = 4
     elseif tipo == "agujereado" then
@@ -87,11 +87,11 @@ function npc:cambiarEstado(tipo)
     elseif tipo == "canon" then
       self.iteradorEstado = 3
     end
-    
+
     self.cooldownIterador = false
-    
+
   elseif self.hp>hp and not self.cooldownIterador then
-    
+
     self.iteradorEstado = 1
     self.cooldownIterador = true
   end
